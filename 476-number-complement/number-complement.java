0812815fcbb61065273be88925a8da0b44828fcf
Harshada Avhad <harshada2576@gmail.com>
2026-05-30 09:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (36.58%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,10 +1,12 @@
 class Solution {
     public int findComplement(int num) 
     {
-        int numberOfBits = (int)(Math.log(num) / Math.log(2)) + 1;
-        
-        int mask = (1 << numberOfBits) - 1;
-        
+        int mask = num;
+        mask |= mask >> 1;
+        mask |= mask >> 2;
+        mask |= mask >> 4;
+        mask |= mask >> 8;
+        mask |= mask >> 16;
         return num ^ mask;
     }
 }
