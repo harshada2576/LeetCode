e9cb97140797f6c2befe54d47f2f53e2b8898389
Harshada Avhad <harshada2576@gmail.com>
2026-06-12 12:04:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (79.09%) - LeetSync

---
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        if (coordinates.length <= 2) {
+            return true; 
+        }
+
+        int x0 = coordinates[0][0];
+        int y0 = coordinates[0][1];
+        int x1 = coordinates[1][0];
+        int y1 = coordinates[1][1];
+
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+
+        for (int i = 2; i < coordinates.length; i++) {
+            int xi = coordinates[i][0];
+            int yi = coordinates[i][1];
+
+            if ((yi - y0) * dx != (xi - x0) * dy) {
+                return false; 
+            }
+        }
+
+        return true; 
+    }
+}
