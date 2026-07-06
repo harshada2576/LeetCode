a91ea426bddc48e3e87d1982c4e01b84bfb1c35e
Harshada Avhad <harshada2576@gmail.com>
2026-07-06 13:47:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (49.64%) - LeetSync

--- 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.java
+++ 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.java
@@ -1,18 +1,7 @@
-class Solution {
-    public int maxContainers(int n, int w, int maxWeight) 
-    {
-        int cells=n*n;
-        if(cells*w <= maxWeight)
-            return cells;
-        else
-            {
-                int c=cells;
-                while(c*w > maxWeight)
-                {
-                    c--;
-                }
-                return c;
-            }
-        
-    }
+class Solution { 
+    public int maxContainers(int n, int w, int maxWeight) { 
+        int cells = n * n; 
+        int possibleContainers = maxWeight / w;
+        return Math.min(cells, possibleContainers);
+    } 
 }
