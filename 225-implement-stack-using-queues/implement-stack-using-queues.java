2a6c28f31af18f14e7cc90417c134c5ef0c400d9
Harshada Avhad <harshada2576@gmail.com>
2026-08-05 15:08:27 +0530

    Time: 1 ms (77.69%) | Memory: 42.5 MB (96.39%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,30 @@
+class MyStack {
+      private Queue<Integer> q;
+
+    public MyStack() {
+      q = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        int size = q.size();
+    q.add(x);
+
+    for (int i = 0; i < size; i++) {
+            q.add(q.poll());
+        }  
+    }
+    
+    public int pop() {
+    return q.poll();  
+    }
+    
+    public int top() {
+        return q.peek();
+ 
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
