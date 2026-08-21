67fea2fd5f8eb1106ef84c9e4d0a46a9ceeeac15
Harshada Avhad <harshada2576@gmail.com>
2026-08-21 22:45:41 +0530

    ✅ Richest Customer Wealth | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 44.6 MB | 2026-08-21

--- leetcode/easy/1672-richest-customer-wealth/solution.java
+++ leetcode/easy/1672-richest-customer-wealth/solution.java
@@ -1,14 +1,12 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int max=Integer.MIN_VALUE,sum=0;
-        for(int i=0; i<accounts.length; i++)
-        {
-            for(int j=0; j<accounts[i].length; j++)
-            {
-                sum+=accounts[i][j];
+        int max = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
             }
-            max=Math.max(max,sum);
-            sum=0;
+            max = Math.max(max, sum);
+            sum = 0;
         }
         return max;
     }
