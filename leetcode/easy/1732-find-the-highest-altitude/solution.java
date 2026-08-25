7c8143fc5e0a645bb9c97cd7dab265ead5209495
Harshada Avhad <harshada2576@gmail.com>
2026-08-25 19:44:26 +0530

    ✅ Find the Highest Altitude | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.2 MB | 2026-08-25

---
+++ leetcode/easy/1732-find-the-highest-altitude/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+      int max=0;
+      int alt=0;
+      for(int i=0; i<gain.length; i++){
+        alt=alt+gain[i];
+        max=Math.max(max,alt);
+      }
+    return max;
+    }
+}
