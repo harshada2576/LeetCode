c25a6104030910db030ad8c2765e6ac66b2a03ac
Harshada Avhad <harshada2576@gmail.com>
2026-08-25 19:06:21 +0530

    ✅ Check if the Sentence Is Pangram | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.1 MB | 2026-08-25

--- leetcode/easy/1832-check-if-the-sentence-is-pangram/solution.java
+++ leetcode/easy/1832-check-if-the-sentence-is-pangram/solution.java
@@ -1,27 +1,12 @@
-import java.util.ArrayList;
-
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        if (sentence.length() < 26) {
-            return false;
-        }
-        if(sentence.length() == 1)
-            return true;
-
-        ArrayList<Character> seen = new ArrayList<>();
-
-        for (char ch : sentence.toCharArray()) {
-            // Add to list only if character isn't already present
-            if (!seen.contains(ch)) {
-                seen.add(ch);
-            }
-            
-            // Early return if all 26 letters are present
-            if (seen.size() == 26) {
-                return true;
+        for(char ch='a';ch<='z';ch++)
+        {
+            if(sentence.indexOf(ch)<0)
+            {
+                return false;
             }
         }
-
-        return seen.size() == 26;
+        return true;
     }
 }
