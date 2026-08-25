2e1fd4f97b4802354da1d74bdfa47d48ccac1306
Harshada Avhad <harshada2576@gmail.com>
2026-08-25 21:11:20 +0530

    ✅ Flipping an Image | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 45.4 MB | 2026-08-25

--- leetcode/easy/0832-flipping-an-image/solution.java
+++ leetcode/easy/0832-flipping-an-image/solution.java
@@ -1,36 +1,26 @@
 class Solution {
-    int[][] invert(int[][] image){
-        for(int i=0; i<image.length; i++){
-            for(int j=0; j<image[i].length; j++){
-                if(image[i][j] == 0)
-                    image[i][j]=1;
-                else
-                    image[i][j]=0;
-            }
-        }
-        return image;
-    }
-    int[][] fliphori(int[][] image) {
-        for (int i = 0; i < image.length; i++) {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+        
+        for (int i = 0; i < n; i++) {
             int left = 0;
             int right = image[i].length - 1;
             
-            // Swap elements from ends moving inward
-            while (left < right) {
-                int temp = image[i][left];
-                image[i][left] = image[i][right];
-                image[i][right] = temp;
-                
+            while (left <= right) {
+                if (left == right) {
+                    // Middle element in an odd-length row: just invert it
+                    image[i][left] ^= 1;
+                } else {
+                    // Swap elements and invert both at the same time
+                    int temp = image[i][left] ^ 1;
+                    image[i][left] = image[i][right] ^ 1;
+                    image[i][right] = temp;
+                }
                 left++;
                 right--;
             }
         }
+        
         return image;
-    }
-    public int[][] flipAndInvertImage(int[][] image) {
-        fliphori(image);
-        invert(image);
-        return image;
-
     }
 }
