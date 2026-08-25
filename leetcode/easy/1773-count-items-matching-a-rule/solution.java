46bda3090f3d7eb8f831379499ead095504bb9e3
Harshada Avhad <harshada2576@gmail.com>
2026-08-25 19:37:39 +0530

    ✅ Count Items Matching a Rule | Easy | Java | Runtime: 3 ms (100.00%) | Memory: 49.8 MB | 2026-08-25

--- leetcode/easy/1773-count-items-matching-a-rule/solution.java
+++ leetcode/easy/1773-count-items-matching-a-rule/solution.java
@@ -1,17 +1,23 @@
-import java.util.List;
-
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int index = 0;
-        if (ruleKey.equals("color")) {
-            index = 1;
-        } else if (ruleKey.equals("name")) {
-            index = 2;
+        //        int count = 0;
+        int n = items.size();
+        int col;
+        if(ruleKey.equals("color")) {
+            col = 1;
+        }
+        else if(ruleKey.equals("name")){
+            col = 2;
+        } 
+        else{
+            col = 0;
         }
 
         int count = 0;
-        for (List<String> item : items) {
-            if (item.get(index).equals(ruleValue)) {
+
+        for(int i = 0 ; i < n;i++ ){
+//            String val = items.get(i).get(col);
+            if (items.get(i).get(col).equals(ruleValue)){
                 count++;
             }
         }
