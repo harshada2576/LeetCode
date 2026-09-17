ec3dd528a619e3f216ec3351abd26cab84051290
Harshada Avhad <harshada2576@gmail.com>
2026-09-17 21:40:27 +0530

    ✅ Find Peak Element | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 44.2 MB | 2026-09-17

---
+++ leetcode/medium/0162-find-peak-element/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+         int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid+1]) {
+                // you are in dec part of array
+                // this may be the ans, but look at left
+                // this is why end != mid - 1
+                end = mid;
+            } else {
+                // you are in asc part of array
+                start = mid + 1; // because we know that mid+1 element > mid element
+            }
+        }
+        return start;
+    }
+}
