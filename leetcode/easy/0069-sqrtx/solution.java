c3a52d90b96a8898573248a2a9d09613f04565fa
Harshada Avhad <harshada2576@gmail.com>
2026-09-21 21:26:17 +0530

    ✅ Sqrt(x) | Easy | Java | Runtime: 1 ms (99.57%) | Memory: 42.8 MB | 2026-09-21

---
+++ leetcode/easy/0069-sqrtx/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        if (x < 2) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x / 2;
+        int answer = 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (mid <= x / mid) {
+                // mid * mid <= x
+                answer = mid;
+                left = mid + 1;
+            } else {
+                // mid * mid > x
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+}
